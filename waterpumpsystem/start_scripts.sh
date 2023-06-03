33f1347a31f6d50afcf314cc8dbc0d51b1368fb9
Emil Asgeirsson <Emil.asgeirsson@gmail.com>
2023-06-03 16:25:00 +0200

    add config files

--- waterpumpsystem/start_scripts.sh
+++ waterpumpsystem/start_scripts.sh
@@ -5,7 +5,7 @@ do
 	/home/pi/waterpumpsystem/read_sensors.sh "${ID}" /dev/ttyACM0 &
 	/home/pi/waterpumpsystem/pump.sh "${ID}" /dev/ttyACM0 &
 	/home/pi/waterpumpsystem/control_pump.sh "${ID}" &
-	/home/pi/waterpumpsystem/farmControl.sh "${ID}" &
+	/home/pi/waterpumpsystem/farm_control.sh "${ID}" &
 done
 
 /home/pi/waterpumpsystem/health.sh &
